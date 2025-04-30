41b2c450c6c4d9d65b04732226b9ef65fa8bebe0
Nick Andriam <raza.jedidia@gmail.com>
2025-04-30 12:45:46 +0300

    feat: Enhance InfoWithIcon component to accept dynamic rating and genres; add new PgText component for displaying PG rating

--- src/components/ui/typography/info-with-icon.tsx
+++ src/components/ui/typography/info-with-icon.tsx
@@ -5,9 +5,15 @@ import Placeholder from "../placeholder/placeholder";
 interface InfoWithIconProps {
   loaded?: boolean;
   placeholderStyle?: string;
+  rating?: number;
+  genres?: string[];
 }
 
-export default function InfoWithIcon(props: InfoWithIconProps) {
+export default function InfoWithIcon({
+  rating,
+  genres,
+  ...props
+}: InfoWithIconProps) {
   // Placeholder for loading state
   if (!props.loaded) {
     return <Placeholder className={`${props.placeholderStyle}`} />;
@@ -16,9 +22,12 @@ export default function InfoWithIcon(props: InfoWithIconProps) {
     <div className="flex items-center gap-2">
       <div className="flex justify-start items-center gap-1">
         <StarIcon className="text-yellow-500" size={14} />
-        <p className="font-extrabold text-sm">4.7</p>
+        <p className="font-extrabold text-sm">{rating || "4.7"}</p>
       </div>
-      <p className="text-xs opacity-60"> | Action • Movie</p>
+      <p className="text-xs opacity-60">
+        {" "}
+        | {genres?.flat().join(" • ") || "Action • Movie"}
+      </p>
     </div>
   );
 }
---
+++ src/components/ui/typography/pg-text.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PgText() {
+  return (
+    <div className="text-base font-bold text-foreground border-2 border-foreground rounded-lg px-2 py-1 w-max opacity-50">
+      PG-13
+    </div>
+  );
+}
