243554f8609c0088ba62b409103049011533eff8
Nick Andriam <raza.jedidia@gmail.com>
2025-05-06 16:52:07 +0300

    fix: Adjust padding in Banner component for improved layout consistency

--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -13,7 +13,7 @@ const Banner = () => {
         fill
         style={{ objectFit: "cover" }}
       />
-      <section className="absolute bottom-0 left-0 w-full h-auto backdrop-blur-xs bg-gradient-to-t from-black to-black/10 p-5 xl:px-40 lg:px-20 md:px-20 sm:px-5 xs:px-5">
+      <section className="absolute bottom-0 left-0 w-full h-auto backdrop-blur-xs bg-gradient-to-t from-black to-black/10 py-5 xl:px-40 lg:px-20 md:px-20 sm:px-5 xs:px-5">
         <Title
           loaded={loaded}
           className="text-white"
@@ -37,7 +37,7 @@ const Banner = () => {
           iusto saepe rem vel, aliquid, quas neque suscipit autem omnis numquam
           ipsum non.
         </Paragraph>
-        <div className="flex gap-4 mt-4 md:w-2/3 lg:w-1/3">
+        <div className="flex gap-4 my-4 md:w-2/3 lg:w-1/3">
           <Button loaded={loaded}>
             <PlayCircle />
             Watch Now
