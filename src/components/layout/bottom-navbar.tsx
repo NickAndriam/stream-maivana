3836f2184538d3605605001252c0ad1684a8b92b
Nick Andriam <raza.jedidia@gmail.com>
2025-05-06 17:09:03 +0300

    fix: Adjust bottom navbar position and border styles for consistency

--- src/components/layout/bottom-navbar.tsx
+++ src/components/layout/bottom-navbar.tsx
@@ -19,7 +19,7 @@ export default function BottomNavbar() {
       : 25;
   return (
     <div
-      className="fixed bottom-5 z-10 left-1/2 -translate-x-1/2 w-auto h-16 bg-background/50 backdrop-blur-sm  flex items-center justify-evenly shadow-md gap-10 rounded-xl border-2 border-foreground/20
+      className="fixed bottom-2 z-10 left-1/2 -translate-x-1/2 w-auto h-16 bg-background/50 backdrop-blur-sm flex items-center justify-evenly shadow-md gap-10 rounded-xl border-1 border-foreground/10
     px-5 py-2"
     >
       <Home className="text-foreground/60" size={iconSize} />
