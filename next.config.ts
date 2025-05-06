ae029db48dabf3fd49e1105c7ebb1497be13b0da
Nick Andriam <raza.jedidia@gmail.com>
2025-05-06 16:52:21 +0300

    fix: Add allowedDevOrigins configuration for local development

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,11 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
   eslint: {
     dirs: ["src"], // Only run ESLint on the "pages" and "components" directories during production builds (next build)
   },
+  allowedDevOrigins: ["http://localhost:3000", "http://192.168.2.73:3000"],
 };
 
 export default nextConfig;
