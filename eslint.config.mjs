fb4dddb2cbfb4cb80d0216c820659597cd306bb9
Nick Andriam <raza.jedidia@gmail.com>
2025-05-07 18:25:35 +0300

    fix: Add @uidotdev/usehooks dependency for improved hooks functionality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
