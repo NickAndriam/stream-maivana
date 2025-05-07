0c7a6556e49828b9e6c9b1e022e34ad2a5f114ac
Nick Andriam <raza.jedidia@gmail.com>
2025-05-07 18:22:07 +0300

    fix: Refactor useScreen hook to utilize useWindowSize for improved responsiveness

--- src/hooks/useScreen.ts
+++ src/hooks/useScreen.ts
@@ -1,41 +1,25 @@
 import React from "react";
-
+import { useWindowSize } from "@uidotdev/usehooks";
 export default function useScreen() {
-  const [screen, setScreen] = React.useState({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0,
-  });
+  const size = useWindowSize();
 
   // Create a default screen size like lg, md, sm, xs
   const screenSize = React.useMemo(() => {
-    if (screen.width >= 1536) {
+    const screenWidth = size.width || 0;
+    console.log("Size", size);
+    if (screenWidth >= 1536) {
       return "2xl"; // Extra large screens
-    } else if (screen.width >= 1280) {
+    } else if (screenWidth >= 1280 && screenWidth < 1536) {
       return "xl"; // Large screens
-    } else if (screen.width >= 1024) {
+    } else if (screenWidth >= 1024 && screenWidth < 1280) {
       return "lg"; // Medium screens
-    } else if (screen.width >= 768) {
+    } else if (screenWidth >= 768 && screenWidth < 1024) {
       return "md"; // Small screens
-    } else if (screen.width >= 640) {
+    } else if (screenWidth >= 640 && screenWidth < 768) {
       return "sm"; // Extra small screens
     } else {
       return "xs"; // Very small screens
     }
-  }, [screen.width]);
-
-  React.useEffect(() => {
-    const handleResize = () => {
-      setScreen({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  }, [size]);
   return screenSize;
 }
